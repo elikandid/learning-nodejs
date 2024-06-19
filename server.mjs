09d6c40ec7d1f0bc1663bd4712bf0f963f1258cd
unknown <elikandid>
2024-06-19 13:11:13 +0000

    Learning nodejs

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import {createServer} from 'node:http';
+
+// Define request handler
+
+//Create HTPP Server
+const server = createServer((req,res) => {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end('<h1>We have an HTP Server</h1>');
+});
+
+//Listen for incoming request
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server is running');
+});
+
+//Normal Function vs  Arrow Function
+function handleRequest() {}
+const handleRequest = () => {}
